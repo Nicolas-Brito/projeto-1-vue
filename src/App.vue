5799538bb5143f1a18ed26d7d92683bbb57ea9dd
Nicolas-Brito <nicolasbyjus@gmail.com>
2024-05-27 19:23:36 -0300

    commit inical

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+<div>
+  <ul>
+   <li v-for="(item,index) in items" :key="index" v-bind:class="{'item-selected':index===itemselecionado}" @mouseover="seleciona(index)" @mouseleave="voltar">
+
+   {{ item }}  
+  </li>
+  </ul>
+</div>
+</template>
+
+<script>
+export default{
+  data(){
+    return{
+    items: ["pao com ovo","hamburguer","pizza","pastel","sushi"],
+    itemselecionado:-1
+    };
+  },
+  methods:{
+    seleciona(index){
+      this.itemselecionado=index
+    },
+    voltar(){
+      this.itemselecionado=-1
+    }
+  }
+}
+</script>
+
+
+<style>
+.item-selected{
+  background-color: red;
+}
+</style>
+
